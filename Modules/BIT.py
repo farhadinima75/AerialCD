9c18b94536e2e7bb09b252300bd9d5de6cc1b688
farhadinima75 <farhadinima75@email.kntu.ac.ir>
2022-05-15 14:48:49 +0430

    Update BIT.py

--- Modules/BIT.py
+++ Modules/BIT.py
@@ -13,6 +13,188 @@ from torch.optim import lr_scheduler
 import functools
 from einops import rearrange
 
+class TwoLayerConv2d(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3):
+        super().__init__(nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size,
+                            padding=kernel_size // 2, stride=1, bias=False),
+                         nn.BatchNorm2d(in_channels),
+                         nn.ReLU(),
+                         nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size,
+                            padding=kernel_size // 2, stride=1)
+                         )
+
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(x, **kwargs) + x
+
+
+class Residual2(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+    def forward(self, x, x2, **kwargs):
+        return self.fn(x, x2, **kwargs) + x
+
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+
+class PreNorm2(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, x2, **kwargs):
+        return self.fn(self.norm(x), self.norm(x2), **kwargs)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+
+class Cross_Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0., softmax=True):
+        super().__init__()
+        inner_dim = dim_head * heads
+        self.heads = heads
+        self.scale = dim ** -0.5
+
+        self.softmax = softmax
+        self.to_q = nn.Linear(dim, inner_dim, bias=False)
+        self.to_k = nn.Linear(dim, inner_dim, bias=False)
+        self.to_v = nn.Linear(dim, inner_dim, bias=False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x, m, mask = None):
+
+        b, n, _, h = *x.shape, self.heads
+        q = self.to_q(x)
+        k = self.to_k(m)
+        v = self.to_v(m)
+
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), [q,k,v])
+
+        dots = torch.einsum('bhid,bhjd->bhij', q, k) * self.scale
+        mask_value = -torch.finfo(dots.dtype).max
+
+        if mask is not None:
+            mask = F.pad(mask.flatten(1), (1, 0), value = True)
+            assert mask.shape[-1] == dots.shape[-1], 'mask has incorrect dimensions'
+            mask = mask[:, None, :] * mask[:, :, None]
+            dots.masked_fill_(~mask, mask_value)
+            del mask
+
+        if self.softmax:
+            attn = dots.softmax(dim=-1)
+        else:
+            attn = dots
+        # attn = dots
+        # vis_tmp(dots)
+
+        out = torch.einsum('bhij,bhjd->bhid', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out = self.to_out(out)
+        # vis_tmp2(out)
+
+        return out
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        self.heads = heads
+        self.scale = dim ** -0.5
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x, mask = None):
+        b, n, _, h = *x.shape, self.heads
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), qkv)
+
+        dots = torch.einsum('bhid,bhjd->bhij', q, k) * self.scale
+        mask_value = -torch.finfo(dots.dtype).max
+
+        if mask is not None:
+            mask = F.pad(mask.flatten(1), (1, 0), value = True)
+            assert mask.shape[-1] == dots.shape[-1], 'mask has incorrect dimensions'
+            mask = mask[:, None, :] * mask[:, :, None]
+            dots.masked_fill_(~mask, mask_value)
+            del mask
+
+        attn = dots.softmax(dim=-1)
+
+
+        out = torch.einsum('bhij,bhjd->bhid', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out = self.to_out(out)
+        return out
+
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Residual(PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout))),
+                Residual(PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout)))
+            ]))
+    def forward(self, x, mask = None):
+        for attn, ff in self.layers:
+            x = attn(x, mask = mask)
+            x = ff(x)
+        return x
+
+
+class TransformerDecoder(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout, softmax=True):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Residual2(PreNorm2(dim, Cross_Attention(dim, heads = heads,
+                                                        dim_head = dim_head, dropout = dropout,
+                                                        softmax=softmax))),
+                Residual(PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout)))
+            ]))
+    def forward(self, x, m, mask = None):
+        """target(query), memory"""
+        for attn, ff in self.layers:
+            x = attn(x, m, mask = mask)
+            x = ff(x)
+        return x
+
+
 model_urls = {
     'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
     'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
@@ -363,7 +545,7 @@ def wide_resnet101_2(pretrained=False, progress=True, **kwargs):
                    pretrained, progress, **kwargs)
 
   
-class ResNet(torch.nn.Module):
+class _ResNet(torch.nn.Module):
     def __init__(self, input_nc, output_nc,
                  resnet_stages_num=5, backbone='resnet18',
                  output_sigmoid=False, if_upsample_2x=True):
@@ -371,7 +553,7 @@ class ResNet(torch.nn.Module):
         In the constructor we instantiate two nn.Linear modules and assign them as
         member variables.
         """
-        super(ResNet, self).__init__()
+        super(_ResNet, self).__init__()
         expand = 1
         if backbone == 'resnet18':
             self.resnet = resnet18(pretrained=True,
@@ -447,7 +629,7 @@ class ResNet(torch.nn.Module):
         return x
       
       
-class BASE_Transformer(ResNet):
+class BASE_Transformer(_ResNet):
     """
     Resnet of 8 downsampling + BIT + bitemporal feature Differencing + a small CNN
     """
@@ -583,7 +765,7 @@ class BASE_Transformer(ResNet):
         outputs.append(x)
         return outputs
  
-def BIT():
-  net = BASE_Transformer(input_nc=3, output_nc=2, token_len=4, resnet_stages_num=4,
+def BIT(InCH=3, OutCH=2):
+  net = BASE_Transformer(input_nc=InCH, output_nc=OutCH, token_len=4, resnet_stages_num=4,
                          with_pos='learned', enc_depth=1, dec_depth=8, decoder_dim_head=8)
   return net
