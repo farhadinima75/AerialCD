3bd49c570d939356292dce492b064ec1ec811992
farhadinima75 <farhadinima75@email.kntu.ac.ir>
2022-06-20 16:18:39 +0430

    Update SNUNet.py

--- Modules/SNUNet.py
+++ Modules/SNUNet.py
@@ -5,6 +5,108 @@
 import torch.nn as nn
 import torch
 
+
+import torch
+import torch.utils.data
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=0, alpha=None, size_average=True):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        if isinstance(alpha, (float, int)):
+            self.alpha = torch.Tensor([alpha, 1-alpha])
+        if isinstance(alpha, list):
+            self.alpha = torch.Tensor(alpha)
+        self.size_average = size_average
+
+    def forward(self, input, target):
+        if input.dim() > 2:
+            # N,C,H,W => N,C,H*W
+            input = input.view(input.size(0), input.size(1), -1)
+
+            # N,C,H*W => N,H*W,C
+            input = input.transpose(1, 2)
+
+            # N,H*W,C => N*H*W,C
+            input = input.contiguous().view(-1, input.size(2))
+
+
+        target = target.view(-1, 1)
+        logpt = F.log_softmax(input, dim=1)
+        logpt = logpt.gather(1, target)
+        logpt = logpt.view(-1)
+        pt = Variable(logpt.data.exp())
+
+        if self.alpha is not None:
+            if self.alpha.type() != input.data.type():
+                self.alpha = self.alpha.type_as(input.data)
+            at = self.alpha.gather(0, target.data.view(-1))
+            logpt = logpt * Variable(at)
+
+        loss = -1 * (1-pt)**self.gamma * logpt
+
+        if self.size_average:
+            return loss.mean()
+        else:
+            return loss.sum()
+
+def dice_loss(logits, true, eps=1e-7):
+    """Computes the Sørensen–Dice loss.
+    Note that PyTorch optimizers minimize a loss. In this
+    case, we would like to maximize the dice loss so we
+    return the negated dice loss.
+    Args:
+        true: a tensor of shape [B, 1, H, W].
+        logits: a tensor of shape [B, C, H, W]. Corresponds to
+            the raw output or logits of the model.
+        eps: added to the denominator for numerical stability.
+    Returns:
+        dice_loss: the Sørensen–Dice loss.
+    """
+    num_classes = logits.shape[1]
+    if num_classes == 1:
+        true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        true_1_hot_f = true_1_hot[:, 0:1, :, :]
+        true_1_hot_s = true_1_hot[:, 1:2, :, :]
+        true_1_hot = torch.cat([true_1_hot_s, true_1_hot_f], dim=1)
+        pos_prob = torch.sigmoid(logits)
+        neg_prob = 1 - pos_prob
+        probas = torch.cat([pos_prob, neg_prob], dim=1)
+    else:
+        true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        probas = F.softmax(logits, dim=1)
+    true_1_hot = true_1_hot.type(logits.type())
+    dims = (0,) + tuple(range(2, true.ndimension()))
+    intersection = torch.sum(probas * true_1_hot, dims)
+    cardinality = torch.sum(probas + true_1_hot, dims)
+    dice_loss = (2. * intersection / (cardinality + eps)).mean()
+    return (1 - dice_loss)
+
+
+def hybrid_loss(predictions, target):
+    """Calculating the loss"""
+    loss = 0
+
+    # gamma=0, alpha=None --> CE
+    focal = FocalLoss(gamma=0, alpha=None)
+
+    for prediction in [predictions]:
+
+        bce = focal(prediction, target)
+        dice = dice_loss(prediction, target)
+        loss += bce + dice
+
+    return loss
+
+
 class conv_block_nested(nn.Module):
     def __init__(self, in_ch, mid_ch, out_ch):
         super(conv_block_nested, self).__init__()
@@ -42,10 +144,26 @@ class up(nn.Module):
         x = self.up(x)
         return x
 
-class Siam_NestedUNet_Conc(nn.Module):
-    # SNUNet-CD without Attention
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_channels, ratio = 16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.fc1 = nn.Conv2d(in_channels,in_channels//ratio,1,bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Conv2d(in_channels//ratio, in_channels,1,bias=False)
+        self.sigmod = nn.Sigmoid()
+    def forward(self,x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmod(out)
+
+class SNUNet_ECAM(nn.Module):
+    # SNUNet-CD with ECAM
     def __init__(self, in_ch=3, out_ch=2):
-        super(Siam_NestedUNet_Conc, self).__init__()
+        super(SNUNet_ECAM, self).__init__()
         torch.nn.Module.dump_patches = True
         n1 = 32     # the initial number of channels of feature map
         filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
@@ -82,11 +200,10 @@ class Siam_NestedUNet_Conc(nn.Module):
 
         self.conv0_4 = conv_block_nested(filters[0] * 5 + filters[1], filters[0], filters[0])
 
-        self.final1 = nn.Conv2d(filters[0], out_ch, kernel_size=1)
-        self.final2 = nn.Conv2d(filters[0], out_ch, kernel_size=1)
-        self.final3 = nn.Conv2d(filters[0], out_ch, kernel_size=1)
-        self.final4 = nn.Conv2d(filters[0], out_ch, kernel_size=1)
-        self.conv_final = nn.Conv2d(out_ch * 4, out_ch, kernel_size=1)
+        self.ca = ChannelAttention(filters[0] * 4, ratio=16)
+        self.ca1 = ChannelAttention(filters[0], ratio=16 // 4)
+
+        self.conv_final = nn.Conv2d(filters[0] * 4, out_ch, kernel_size=1)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -124,13 +241,14 @@ class Siam_NestedUNet_Conc(nn.Module):
         x1_3 = self.conv1_3(torch.cat([x1_0A, x1_0B, x1_1, x1_2, self.Up2_2(x2_2)], 1))
         x0_4 = self.conv0_4(torch.cat([x0_0A, x0_0B, x0_1, x0_2, x0_3, self.Up1_3(x1_3)], 1))
 
+        out = torch.cat([x0_1, x0_2, x0_3, x0_4], 1)
 
-        output1 = self.final1(x0_1)
-        output2 = self.final2(x0_2)
-        output3 = self.final3(x0_3)
-        output4 = self.final4(x0_4)
-        output = self.conv_final(torch.cat([output1, output2, output3, output4], 1))
-        return output
+        intra = torch.sum(torch.stack((x0_1, x0_2, x0_3, x0_4)), dim=0)
+        ca1 = self.ca1(intra)
+        out = self.ca(out) * (out + ca1.repeat(1, 4, 1, 1))
+        out = self.conv_final(out)
+
+        return out
 
 def SNUNet(InCH=3, OutCH=2):
-  return Siam_NestedUNet_Conc(in_ch=InCH, out_ch=OutCH)
+  return SNUNet_ECAM(in_ch=InCH, out_ch=OutCH)
